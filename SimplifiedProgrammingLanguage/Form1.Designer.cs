6dfe33c68429048986ebdd221160694050fbd8a6
Sarang Sohail <sarangsohail@gmail.com>
2019-01-06 14:03:32 +0000

    pen status button added - yet to implement tho.

--- SimplifiedProgrammingLanguage/Form1.Designer.cs
+++ SimplifiedProgrammingLanguage/Form1.Designer.cs
@@ -31,7 +31,16 @@
             this.commandInputter = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.drawingCanvas = new System.Windows.Forms.PictureBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clearFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.penStatusButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.drawingCanvas)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // commandInputter
@@ -55,26 +64,96 @@
             // 
             // drawingCanvas
             // 
+            this.drawingCanvas.BackColor = System.Drawing.Color.White;
             this.drawingCanvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.drawingCanvas.Location = new System.Drawing.Point(12, 12);
+            this.drawingCanvas.Location = new System.Drawing.Point(12, 44);
             this.drawingCanvas.Name = "drawingCanvas";
-            this.drawingCanvas.Size = new System.Drawing.Size(1308, 408);
+            this.drawingCanvas.Size = new System.Drawing.Size(1308, 376);
             this.drawingCanvas.TabIndex = 2;
             this.drawingCanvas.TabStop = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(117, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(117, 26);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(117, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.clearFormToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1368, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clearFormToolStripMenuItem
+            // 
+            this.clearFormToolStripMenuItem.Name = "clearFormToolStripMenuItem";
+            this.clearFormToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
+            this.clearFormToolStripMenuItem.Text = "Clear Form";
+            this.clearFormToolStripMenuItem.Click += new System.EventHandler(this.clearFormToolStripMenuItem_Click);
+            // 
+            // penStatusButton
+            // 
+            this.penStatusButton.BackColor = System.Drawing.Color.Red;
+            this.penStatusButton.Location = new System.Drawing.Point(679, 437);
+            this.penStatusButton.Name = "penStatusButton";
+            this.penStatusButton.Size = new System.Drawing.Size(92, 32);
+            this.penStatusButton.TabIndex = 5;
+            this.penStatusButton.UseVisualStyleBackColor = false;
+            this.penStatusButton.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1368, 510);
+            this.Controls.Add(this.penStatusButton);
             this.Controls.Add(this.drawingCanvas);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.commandInputter);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.drawingCanvas)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +164,14 @@
         private System.Windows.Forms.TextBox commandInputter;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox drawingCanvas;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearFormToolStripMenuItem;
+        private System.Windows.Forms.Button penStatusButton;
     }
 }
 
--- SimplifiedProgrammingLanguage/Form1.cs
+++ SimplifiedProgrammingLanguage/Form1.cs
@@ -10,7 +10,7 @@ namespace SimplifiedProgrammingLanguage
     {
 
         ArrayList shapesArray = new ArrayList();
-        private PointF Pen { get; set; }
+        private PointF penLocation { get; set; }
 
         Graphics g; 
 
@@ -18,7 +18,7 @@ namespace SimplifiedProgrammingLanguage
         {
             InitializeComponent();
             g = drawingCanvas.CreateGraphics();
-            Pen = new Point(0, 0);
+            penLocation = new Point(0, 0);
             g.Clear(Color.White);
         }
 
@@ -43,18 +43,19 @@ namespace SimplifiedProgrammingLanguage
                 string line = commands[i];
                 string[] singleLineCommand = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
 
+
                 if (singleLineCommand[0].Equals("moveTo"))
                 {
-                    try
-                    {
-                        string[] param = singleLineCommand[1].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-                        Pen = new PointF(float.Parse(param[0]), float.Parse(param[1]));
+                        try
+                        {
+                            string[] param = singleLineCommand[1].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                            penLocation = new PointF(float.Parse(param[0]), float.Parse(param[1]));
+
                     }
                     catch (Exception ee)
-                    {
-                        MessageBox.Show(ee.Message);
-                            
-                    }
+                        {
+                            MessageBox.Show(ee.Message);
+                        }
                     
                 }
 
@@ -62,9 +63,9 @@ namespace SimplifiedProgrammingLanguage
                     {
                         try
                         {
-                            string command = singleLineCommand[1];
+                        string command = singleLineCommand[1];
                             Shape shape = shapeGenerator.GetShape(singleLineCommand[0]);
-                            shape.Set(Pen.X, Pen.Y, float.Parse(command));
+                            shape.Set(penLocation.X, penLocation.Y, float.Parse(command));
                             shapesArray.Add(shape);
                             shape.Draw(g);
 
@@ -83,7 +84,7 @@ namespace SimplifiedProgrammingLanguage
                         string param = singleLineCommand[1];
                         string[] para = param.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
                         Shape shapes = shapeGenerator.GetShape(singleLineCommand[0]);
-                        shapes.Set(Pen.X, Pen.Y, float.Parse(para[0]), float.Parse(para[1]));
+                        shapes.Set(penLocation.X, penLocation.Y, float.Parse(para[0]), float.Parse(para[1]));
                         shapesArray.Add(shapes);
                         shapes.Draw(g);
                     }
@@ -101,7 +102,7 @@ namespace SimplifiedProgrammingLanguage
                             string shapeCommand = singleLineCommand[1];
                             string[] command = shapeCommand.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
                             Shape shape = shapeGenerator.GetShape(singleLineCommand[0]);
-                            shape.Set(Pen.X, Pen.Y, float.Parse(command[0]), float.Parse(command[1]));
+                            shape.Set(penLocation.X, penLocation.Y, float.Parse(command[0]), float.Parse(command[1]));
                             shapesArray.Add(shape);
                             shape.Draw(g);
                     }
@@ -116,16 +117,16 @@ namespace SimplifiedProgrammingLanguage
                 {
                     try
                     {
-                        string param = singleLineCommand[1];
-                        string[] para = param.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                        string command = singleLineCommand[1];
+                        string[] para = command.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
                         float[] Points = new float[para.Length + 2];
-                        Points[0] = Pen.X;
-                        Points[1] = Pen.Y;
+                        Points[0] = penLocation.X;
+                        Points[1] = penLocation.Y;
                         for (int j = 2; j < Points.Length; j++)
                         {
                             Points[j] = float.Parse(para[j - 2]);
                         }
-                 
+
                             Shape shape = shapeGenerator.GetShape(singleLineCommand[0]);
                             shape.Set(Points);
                             shapesArray.Add(shape);
@@ -150,11 +151,23 @@ namespace SimplifiedProgrammingLanguage
                 }
                 else
                 {
-                    Console.WriteLine("invalid shape in array");
+                    Console.WriteLine("Wrong shape in array");
                 }
             }
         }
 
+     
+
+        private void clearFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
